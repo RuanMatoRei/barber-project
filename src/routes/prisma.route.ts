75b425bcebc091350cd245b263e112e8ecc9d12d
RuanMatoRei <132849395+RuanMatoRei@users.noreply.github.com>
2026-01-31 11:52:13 -0300

    Delete src directory

--- src/routes/prisma.route.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance } from 'fastify';
-
-export async function prismaRoutes(app: FastifyInstance) {
-    app.get('/prisma', async () => {
-        return {
-            prisma: 'ok'
-        };
-    });
-}
--- src/server.ts
+++
@@ -1,8 +0,0 @@
-// backend/src/server.ts
-import { app } from './app.js'
-
-const port = Number(process.env.PORT) || 3333;
-
-app.listen({ port }).then(() => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
